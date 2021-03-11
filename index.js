9b1ec1b22c83a77981c36ef14c6de962f1177180
devadharshini30 <54495417+devadharshini30@users.noreply.github.com>
2021-03-11 23:25:17 +0530

    Web App Code Updated
    Web App Code Updated

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const router = express.Router();
 const port = 3000
 
-router.get('/dash', (req, res) => {
-    res.sendFile('index.html')
-})
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('/assets'))
 
-app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
